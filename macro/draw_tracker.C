167629c8767c0d8a2b32be9bf16e92a4fffaa5c8
rdom <r.dzhygadlo@gsi.de>
2023-06-06 19:26:17 +0200

    plot tracker's hits

--- macro/draw_tracker.C
+++ macro/draw_tracker.C
@@ -6,13 +6,14 @@ void draw_tracker(TString infile = "../build/hits.root") {
 
   TH2F *hTracker1 = new TH2F("hTracker", "hTracker", 200, 0, 100, 200, 0, 100);
 
-  
-  while (t.next() && t.i() < 10000) {
+  while (t.next()) {
+    // get position vectors
     TVector3 t1 = t.event()->getT1Position();
     TVector3 t2 = t.event()->getT2Position();
     TVector3 t3 = t.event()->getT3Position();
-    
-    hTracker1->Fill(t1.X(),t1.Y());
+
+    // fill 2D histogram
+    hTracker1->Fill(t1.X(), t1.Y());
   }
 
   t.add_canvas("t1");
